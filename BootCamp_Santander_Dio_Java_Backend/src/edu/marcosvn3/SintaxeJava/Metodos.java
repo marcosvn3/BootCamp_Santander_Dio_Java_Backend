6519c24c7af35e569e204fda2dcf343c53222d51
Marcos Santos <marcosvn3@academico.ufs.br>
2023-08-22 21:39:38 -0300

    Explicando oque é um metodo.

---
+++ BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Metodos.java
@@ -0,0 +1,18 @@
+package edu.marcosvn3.SintaxeJava;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		//Metodos são como uma ação que a classe pode exercer
+		// Estrutura:
+			//TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+		//Ex:
+			//int somar (int numeroUm, int numero2)
+			//String formatarCep (long cep)
+		
+		//OBS:Como parte da estrutura de declaração de variáveis e métodos,
+		//também temos o aspecto da visibilidade, mas ainda não é necessário nesta etapa de estudos.
+	}
+
+}
