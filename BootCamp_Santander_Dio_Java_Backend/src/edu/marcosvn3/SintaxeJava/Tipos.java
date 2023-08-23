bfb90a1e3004af7685417d8aa66b8fd240b18c2b
Marcos Santos <marcosvn3@academico.ufs.br>
2023-08-23 20:54:00 -0300

    Tipos De Dados.

--- BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Tipos.java
+++ BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Tipos.java
@@ -6,8 +6,8 @@ public class Tipos {
 		byte idade = 123;
 		short ano = 2021;
 		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
-		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
-		float pi = 3.14F;
+		long cpf = 98765432109L; // Sempre termina em LetraL para definir tipo,se começar com zero, talvez tenha que ser outro tipo.
+		float pi = 3.14F;// sempre termina em Letra F para definir tipo.
 		double salario = 1275.33;
 		
 		
