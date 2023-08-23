710ca5d88217498c7b86ce717f8f912025c1ee30
Marcos Santos <marcosvn3@academico.ufs.br>
2023-08-22 21:46:13 -0300

    Atribuindo exemplo de metodo

--- BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Metodos.java
+++ BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Metodos.java
@@ -1,6 +1,11 @@
 package edu.marcosvn3.SintaxeJava;
 
 public class Metodos {
+	
+	//Ex metodo:
+	public static void mensagem(){
+        System.out.println("Curso Java Progressivo!");
+    }
 
 	public static void main(String[] args) {
 		//Metodos são como uma ação que a classe pode exercer
@@ -13,6 +18,11 @@ public class Metodos {
 		
 		//OBS:Como parte da estrutura de declaração de variáveis e métodos,
 		//também temos o aspecto da visibilidade, mas ainda não é necessário nesta etapa de estudos.
+		
+		
+		System.out.print("Exibindo a mensagem uma vez: ");
+		//chamando metodo:
+        mensagem();
 	}
 
 }
