3490434669512e75db4e68aaddbb4cc9b846bd3c
Marcos Santos <marcosvn3@academico.ufs.br>
2023-08-23 20:52:42 -0300

    Tipos De Dados!

---
+++ BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Tipos.java
@@ -0,0 +1,25 @@
+package edu.marcosvn3.SintaxeJava;
+
+public class Tipos {
+	
+	public static void main(String[] args) {
+		byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+		
+		
+		// Um tipo menor pde ser atribuido a um tipo maior:
+			int novaIdade = idade;
+			
+		//porem para um tipo maior atribuir um tipo menor é preciso fazer um CAST
+		//Cast é a confirmacao de troca de tipo mesmo com perca de informacoes
+		//ex:
+			int novoCPF = (int) cpf;
+		//                  ^
+			//            |CAST|
+			
+	}
+}
