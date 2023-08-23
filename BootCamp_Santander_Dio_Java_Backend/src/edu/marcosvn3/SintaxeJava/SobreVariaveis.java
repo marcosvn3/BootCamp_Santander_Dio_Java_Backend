2b08814c1adabacfb6ae19487c3a73dcbf49a71e
Marcos Santos <marcosvn3@academico.ufs.br>
2023-08-22 21:14:49 -0300

    Aprendendo sobre variaveis!

--- BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/SobreVariaveis.java
+++ BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/SobreVariaveis.java
@@ -3,6 +3,12 @@ package edu.marcosvn3.SintaxeJava;
 public class SobreVariaveis {
 
 	public static void main(String[] args) {
+		//Variaveis seguem o padrao camelCase
+		//Variaveis com nomes compostos
+		// ex:
+				int anoFabricacao;
+				String nomeTitular;
+		
 		//variaveis seguem uma estrutura Base
 		// Tipo nomeVariavel = valor/atribuição
 		// ex:
