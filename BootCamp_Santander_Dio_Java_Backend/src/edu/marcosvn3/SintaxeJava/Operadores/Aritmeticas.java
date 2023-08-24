1c08193b7269475230b8ac2c919fa517cdfd0e7f
Marcos Santos <marcosvn3@academico.ufs.br>
2023-08-24 20:58:03 -0300

    Aprendendo sobre Operadores!

---
+++ BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Operadores/Aritmeticas.java
@@ -0,0 +1,21 @@
+package edu.marcosvn3.SintaxeJava.Operadores;
+
+public class Aritmeticas {
+
+	public static void main(String[] args) {
+		//O operador aritmético, é utilizado para realizar operações matemáticas entre valores numéricos,
+		//podendo se tornar ou não uma expressão mais complexa.
+		//Os operadores aritméticos são: + (adição), - (subtração), * (multiplicação) e / (divisão).
+		
+		double soma = 10.5 + 15.7;
+		int subtração = 113 - 25;
+		int multiplicacao = 20 * 7;
+		int divisao = 15 / 3;
+		int modulo = 18 % 3;
+		double resultado = (10 * 7) + (20/4);
+		
+		//O operador de adição (+), quando utilizado em variáveis do tipo texto, realizará a “concatenação de textos”.
+
+	}
+
+}
---
+++ BootCamp_Santander_Dio_Java_Backend/src/edu/marcosvn3/SintaxeJava/Operadores/Relacionais.java
@@ -0,0 +1,37 @@
+package edu.marcosvn3.SintaxeJava.Operadores;
+
+public class Relacionais {
+
+	public static void main(String[] args) {
+		// Os operadores relacionais, avaliam a relação entre duas variáveis ou expressões. 
+		//Neste caso, mais precisamente, definem se o operando à esquerda é igual, diferente, menor, menor ou igual,
+		//maior, maior ou igual ao da direita, retornando um valor booleano como resultado.
+		
+		//	== Quando desejamos verificar se uma variável é IGUAL A outra.
+		//	!= Quando desejamos verificar se uma variável é DIFERENTE da outra.
+		//	> Quando desejamos verificar se uma variável é MAIOR QUE a outra.
+		//	>= Quando desejamos verificar se uma variável é MAIOR OU IGUAL a outra.
+		//	< Quando desejamos verificar se uma variável é MENOR QUE outra.
+		//	<= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra.
+		
+		//classe Operadores.java
+		int numero1 = 1;
+		int numero2 = 2;
+
+		if(numero1 > numero2)
+			System.out.print("Numero 1 maior que numero 2");
+
+		if(numero1 < numero2)
+			System.out.print("Numero 1 menor que numero 2");
+
+		if(numero1 >= numero2)
+			System.out.print("Numero 1 maior ou igual que numero 2");
+
+		if(numero1 <= numero2)
+			System.out.print("Numero 1 menor ou igual que numero 2");
+
+		if(numero1 != numero2)
+			System.out.print("Numero 1 é diferente de numero 2");
+	}
+
+}
